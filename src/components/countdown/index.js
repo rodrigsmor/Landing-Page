adff3b283c1f46e7d0a70af5072a0a4f79d79d94
RodrigoMoreiraDaSilva <rodrigomoreiradasilva.of@gmail.com>
2021-07-20 11:25:52 -0300

    countdown working

--- src/components/countdown/index.js
+++ src/components/countdown/index.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as S from './styled';
 
 export default function Countdown() {
+    const [timerDays, setTimerDays] = useState();
+    const [timerHours, setTimerHours] = useState();
+    const [timerMinutes, setTimerMinutes] = useState();
+    const [timerSeconds, setTimerSeconds] = useState();
+
+    let interval = useRef();
+    const startTimer = () => {
+        const countdownDate = new Date('November 26 2021 00:00:00').getTime();
+
+        interval = setInterval(() => {
+            const now = new Date().getTime();
+            const distance = countdownDate - now;
+
+            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
+            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            if(distance < 0) {
+                clearInterval(interval.current);
+            } else {
+                setTimerDays(days);
+                setTimerHours(hours);
+                setTimerMinutes(minutes)
+                setTimerSeconds(seconds);
+            }
+        }, 1000);
+    };
+
+    useEffect(() => {
+        startTimer();
+        return () => {
+            clearInterval(interval.current);
+        };
+    });
+
     return (
         <S.section>
             <S.countdownWrapper>
@@ -12,25 +48,25 @@ export default function Countdown() {
 
                 <div className="countdown">
                     <S.box>
-                        <h3>80</h3>
+                        <h3>{timerDays}</h3>
                         <p>Dias</p>
                     </S.box>
                     <span>:</span>
 
                     <S.box>
-                        <h3>80</h3>
+                        <h3>{timerHours}</h3>
                         <p>Horas</p>
                     </S.box>
                     <span>:</span>
 
                     <S.box>
-                        <h3>80</h3>
+                        <h3>{timerMinutes}</h3>
                         <p>Minutos</p>
                     </S.box>
                     <span>:</span>
 
                     <S.box>
-                        <h3>80</h3>
+                        <h3>{timerSeconds}</h3>
                         <p>Segundos</p>
                     </S.box>
                 </div>
